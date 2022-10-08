881a96a98fbbb3faec624bb73595d40b94e2b96e
Lil s0uxz <gkh.0546@gmail.com>
2022-10-08 17:41:54 +0700

    #public_01

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pacifico': ["'Pacifico'", 'cursive'],
+    },
   },
+},
   plugins: [],
 }
